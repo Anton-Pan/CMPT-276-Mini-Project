fa5e2775d44f5c211615063afbf37fd59b348f6e
Anton-Pan <91840673+Anton-Pan@users.noreply.github.com>
2025-06-03 22:55:34 -0700

    Added text to header - Added my own name as main text - Added "CMPT 276 Mini-Project" as subtitle text
    Relevant Issue(s):
    https://github.com/Anton-Pan/CMPT-276-Mini-Project/issues/1?issue=Anton-Pan%7CCMPT-276-Mini-Project%7C3

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,15 @@ function App() {
     const Header = () => {
         return (
             <section style={{display: "flex"}}>
+                <header>
+                    <h1>
+                        Anton Panfilenok
+                    </h1>
+                    <h5>
+                        CMPT 276 Mini-Project
+                    </h5>
+                </header>
+
 
             </section>
         )
