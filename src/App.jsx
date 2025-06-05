a6da62813c30eaa506f676ddcba82a18b90f33a0
Anton-Pan <91840673+Anton-Pan@users.noreply.github.com>
2025-06-04 22:04:01 -0700

    Created footer container - Manually styled footer container to look like wireframe design - Moved the dark mode toggle switch into the Header component Relevant Issue(s): https://github.com/Anton-Pan/CMPT-276-Mini-Project/issues/1?issue=Anton-Pan%7CCMPT-276-Mini-Project%7C15

--- src/App.jsx
+++ src/App.jsx
@@ -2,104 +2,104 @@ import './App.css'
 import './darkModeToggleSwitch.css'
 
 function App() {
-    const darkModeToggleSwitch = () => {
-        // Credit to https://uiverse.io/RiccardoRapelli/jolly-chicken-91 (MIT License) for checkbox design, used in the return statement of this function and ./darkModeToggleSwitch.css
-        // Credit to https://digitallytailored.github.io/Classless.css/ for checkbox functionality, used in the return statement of this function
-        // Both the design and functionlity were modified by Anton-Pan
-        document.addEventListener('DOMContentLoaded', () => {
-            const darkModeToggle = document.getElementById('darkModeToggle');
-
-            function setColorScheme(scheme) {
-                document.documentElement.setAttribute('color-scheme', scheme);
-                localStorage.setItem('color-scheme', scheme);
-            }
-
-            function getColorScheme() {
-                let scheme = localStorage.getItem('color-scheme');
-                if (scheme) {
-                    return scheme;
-                }
-                return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
-            }
-
-            setColorScheme(getColorScheme());
-
-            darkModeToggle.addEventListener('change', function () {
-                setColorScheme(this.checked ? 'dark' : 'light');
-            });
-
-            darkModeToggle.checked = getColorScheme() === 'dark';
-        })
-        return (
-            <label className="switch">
-                <input type="checkbox" id="darkModeToggle"/>
-                <div className="slider round">
-                    <div className="sun-moon">
-                        <svg id="moon-dot-1" className="moon-dot" viewBox="0 0 100 100">
-                            <circle cx="50" cy="50" r="50"></circle>
-                        </svg>
-                        <svg id="moon-dot-2" className="moon-dot" viewBox="0 0 100 100">
-                            <circle cx="50" cy="50" r="50"></circle>
-                        </svg>
-                        <svg id="moon-dot-3" className="moon-dot" viewBox="0 0 100 100">
-                            <circle cx="50" cy="50" r="50"></circle>
-                        </svg>
-                        <svg id="light-ray-1" className="light-ray" viewBox="0 0 100 100">
-                            <circle cx="50" cy="50" r="50"></circle>
-                        </svg>
-                        <svg id="light-ray-2" className="light-ray" viewBox="0 0 100 100">
-                            <circle cx="50" cy="50" r="50"></circle>
-                        </svg>
-                        <svg id="light-ray-3" className="light-ray" viewBox="0 0 100 100">
-                            <circle cx="50" cy="50" r="50"></circle>
-                        </svg>
-
-                        <svg id="cloud-1" className="cloud-dark" viewBox="0 0 100 100">
-                            <circle cx="50" cy="50" r="50"></circle>
-                        </svg>
-                        <svg id="cloud-2" className="cloud-dark" viewBox="0 0 100 100">
-                            <circle cx="50" cy="50" r="50"></circle>
-                        </svg>
-                        <svg id="cloud-3" className="cloud-dark" viewBox="0 0 100 100">
-                            <circle cx="50" cy="50" r="50"></circle>
-                        </svg>
-                        <svg id="cloud-4" className="cloud-light" viewBox="0 0 100 100">
-                            <circle cx="50" cy="50" r="50"></circle>
-                        </svg>
-                        <svg id="cloud-5" className="cloud-light" viewBox="0 0 100 100">
-                            <circle cx="50" cy="50" r="50"></circle>
-                        </svg>
-                        <svg id="cloud-6" className="cloud-light" viewBox="0 0 100 100">
-                            <circle cx="50" cy="50" r="50"></circle>
-                        </svg>
-                    </div>
-                    <div className="stars">
-                        <svg id="star-1" className="star" viewBox="0 0 20 20">
-                            <path
-                                d="M 0 10 C 10 10,10 10 ,0 10 C 10 10 , 10 10 , 10 20 C 10 10 , 10 10 , 20 10 C 10 10 , 10 10 , 10 0 C 10 10,10 10 ,0 10 Z"
-                            ></path>
-                        </svg>
-                        <svg id="star-2" className="star" viewBox="0 0 20 20">
-                            <path
-                                d="M 0 10 C 10 10,10 10 ,0 10 C 10 10 , 10 10 , 10 20 C 10 10 , 10 10 , 20 10 C 10 10 , 10 10 , 10 0 C 10 10,10 10 ,0 10 Z"
-                            ></path>
-                        </svg>
-                        <svg id="star-3" className="star" viewBox="0 0 20 20">
-                            <path
-                                d="M 0 10 C 10 10,10 10 ,0 10 C 10 10 , 10 10 , 10 20 C 10 10 , 10 10 , 20 10 C 10 10 , 10 10 , 10 0 C 10 10,10 10 ,0 10 Z"
-                            ></path>
-                        </svg>
-                        <svg id="star-4" className="star" viewBox="0 0 20 20">
-                            <path
-                                d="M 0 10 C 10 10,10 10 ,0 10 C 10 10 , 10 10 , 10 20 C 10 10 , 10 10 , 20 10 C 10 10 , 10 10 , 10 0 C 10 10,10 10 ,0 10 Z"
-                            ></path>
-                        </svg>
-                    </div>
-                </div>
-            </label>
-        )
-    }
     const Header = () => {
+        const darkModeToggleSwitch = () => {
+            // Credit to https://uiverse.io/RiccardoRapelli/jolly-chicken-91 (MIT License) for checkbox design, used in the return statement of this function and ./darkModeToggleSwitch.css
+            // Credit to https://digitallytailored.github.io/Classless.css/ for checkbox functionality, used in the return statement of this function
+            // Both the design and functionlity were modified by Anton-Pan
+            document.addEventListener('DOMContentLoaded', () => {
+                const darkModeToggle = document.getElementById('darkModeToggle');
+
+                function setColorScheme(scheme) {
+                    document.documentElement.setAttribute('color-scheme', scheme);
+                    localStorage.setItem('color-scheme', scheme);
+                }
+
+                function getColorScheme() {
+                    let scheme = localStorage.getItem('color-scheme');
+                    if (scheme) {
+                        return scheme;
+                    }
+                    return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+                }
+
+                setColorScheme(getColorScheme());
+
+                darkModeToggle.addEventListener('change', function () {
+                    setColorScheme(this.checked ? 'dark' : 'light');
+                });
+
+                darkModeToggle.checked = getColorScheme() === 'dark';
+            })
+            return (
+                <label className="switch">
+                    <input type="checkbox" id="darkModeToggle"/>
+                    <div className="slider round">
+                        <div className="sun-moon">
+                            <svg id="moon-dot-1" className="moon-dot" viewBox="0 0 100 100">
+                                <circle cx="50" cy="50" r="50"></circle>
+                            </svg>
+                            <svg id="moon-dot-2" className="moon-dot" viewBox="0 0 100 100">
+                                <circle cx="50" cy="50" r="50"></circle>
+                            </svg>
+                            <svg id="moon-dot-3" className="moon-dot" viewBox="0 0 100 100">
+                                <circle cx="50" cy="50" r="50"></circle>
+                            </svg>
+                            <svg id="light-ray-1" className="light-ray" viewBox="0 0 100 100">
+                                <circle cx="50" cy="50" r="50"></circle>
+                            </svg>
+                            <svg id="light-ray-2" className="light-ray" viewBox="0 0 100 100">
+                                <circle cx="50" cy="50" r="50"></circle>
+                            </svg>
+                            <svg id="light-ray-3" className="light-ray" viewBox="0 0 100 100">
+                                <circle cx="50" cy="50" r="50"></circle>
+                            </svg>
+
+                            <svg id="cloud-1" className="cloud-dark" viewBox="0 0 100 100">
+                                <circle cx="50" cy="50" r="50"></circle>
+                            </svg>
+                            <svg id="cloud-2" className="cloud-dark" viewBox="0 0 100 100">
+                                <circle cx="50" cy="50" r="50"></circle>
+                            </svg>
+                            <svg id="cloud-3" className="cloud-dark" viewBox="0 0 100 100">
+                                <circle cx="50" cy="50" r="50"></circle>
+                            </svg>
+                            <svg id="cloud-4" className="cloud-light" viewBox="0 0 100 100">
+                                <circle cx="50" cy="50" r="50"></circle>
+                            </svg>
+                            <svg id="cloud-5" className="cloud-light" viewBox="0 0 100 100">
+                                <circle cx="50" cy="50" r="50"></circle>
+                            </svg>
+                            <svg id="cloud-6" className="cloud-light" viewBox="0 0 100 100">
+                                <circle cx="50" cy="50" r="50"></circle>
+                            </svg>
+                        </div>
+                        <div className="stars">
+                            <svg id="star-1" className="star" viewBox="0 0 20 20">
+                                <path
+                                    d="M 0 10 C 10 10,10 10 ,0 10 C 10 10 , 10 10 , 10 20 C 10 10 , 10 10 , 20 10 C 10 10 , 10 10 , 10 0 C 10 10,10 10 ,0 10 Z"
+                                ></path>
+                            </svg>
+                            <svg id="star-2" className="star" viewBox="0 0 20 20">
+                                <path
+                                    d="M 0 10 C 10 10,10 10 ,0 10 C 10 10 , 10 10 , 10 20 C 10 10 , 10 10 , 20 10 C 10 10 , 10 10 , 10 0 C 10 10,10 10 ,0 10 Z"
+                                ></path>
+                            </svg>
+                            <svg id="star-3" className="star" viewBox="0 0 20 20">
+                                <path
+                                    d="M 0 10 C 10 10,10 10 ,0 10 C 10 10 , 10 10 , 10 20 C 10 10 , 10 10 , 20 10 C 10 10 , 10 10 , 10 0 C 10 10,10 10 ,0 10 Z"
+                                ></path>
+                            </svg>
+                            <svg id="star-4" className="star" viewBox="0 0 20 20">
+                                <path
+                                    d="M 0 10 C 10 10,10 10 ,0 10 C 10 10 , 10 10 , 10 20 C 10 10 , 10 10 , 20 10 C 10 10 , 10 10 , 10 0 C 10 10,10 10 ,0 10 Z"
+                                ></path>
+                            </svg>
+                        </div>
+                    </div>
+                </label>
+            )
+        }
         return (
             <section style={{display: "flex"}}>
                 <header>
@@ -139,9 +139,18 @@ function App() {
             </section>
         )
     }
+
+    const Footer = () => {
+        return (
+            <section style={{display: "flex", alignItems: "center", justifyContent: "space-around"}}>
+
+            </section>
+        )
+    }
     return (
         <>
             <Header/>
+            <Footer/>
         </>
     )
 }
